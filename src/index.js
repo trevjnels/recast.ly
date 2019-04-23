2003868fe5691c82d8ca36291621bb3c0403c25c
trevjnels <trevorjohnnels@gmail.com>
2019-04-23 15:24:22 -0700

    maybe stuck with detatched head

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import App from './components/App.js';
 import exampleVideoData from './data/exampleVideoData.js';
 
 ReactDOM.render(<App data={exampleVideoData}/>, document.getElementById('app'));
+//  comment
